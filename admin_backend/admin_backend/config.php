96f17dc8d703133874825f1f5823cdf5e3a63089
swirlwow <littlewow@gmail.com>
2026-04-15 03:14:12 +0800

    Delete admin_backend directory

--- admin_backend/admin_backend/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Taipei');
-
-define('GAS_EXEC_URL', 'https://script.google.com/macros/s/AKfycbweZp4YI8Z2ianOCxeaJP_tIZPjRJp4f_B7Z7nXbG0qXUktcccCHiGHWN_erFN9CuWT/exec');
-
-// 若之後要加簡易登入，可改成 true
-define('ADMIN_AUTH_ENABLED', false);
-define('ADMIN_USERNAME', 'admin');
-define('ADMIN_PASSWORD', 'changeme');
